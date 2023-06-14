bc421e229c93d401a82fa7c91debfe430d1d5953
Manlosmade <encryptforwarding@duck.com>
2023-06-14 21:28:53 +0100

    Finished my typewriter function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,37 +11,27 @@ let i = 0
 let speed = 100
 
 
-/*function Typewrite(Alpha, Beta, Beta1) {
-    if(i < Beta.length) {
-        Alpha.innerHTML += Beta.charAt(i);
-        i++;
-        setTimeout(Typewrite, speed, Alpha, Beta)
-        console.log("TEST" + i)
-
-    } else if(i == Beta.length) {
-        Alpha.innerHTML = Alpha.innerHTML.slice(0, -1);
-        setTimeout(Typewrite, speed, Alpha, Beta)
-    } else if(i== 0 ) {
-        Alpha.innerHTML += Beta1.charAt(i);
-        i++;
-        setTimeout(Typewrite, speed, Alpha, Beta)
-        console.log("TEST" + i)
-    }
-}
-
-Typewrite(typewriterContainer, message[0], message[1])
-*/
 
 async function Typewriter(containerElement, text) {
+    TypewriteAlpha()
+    await sleep(1350)
+    TypewriteBeta()
+    async function TypewriteAlpha() {
     for(let i = 0; i < text.length; i++) {
         containerElement.innerHTML += text.charAt(i);      
         await sleep(100)
-    } 
+    }
+}
+async function TypewriteBeta() {
     for(let i = 0; i != text.length; i++) {
         containerElement.innerHTML = containerElement.innerHTML.slice(0, -1) 
-        await sleep(100)
-    }
+        await sleep(50)
+    }}
 
 }
 
 Typewriter(typewriterContainer, message[0])
+await sleep(2500)
+Typewriter(typewriterContainer, message[1])
+await sleep(2500)
+Typewriter(typewriterContainer, message[2])
